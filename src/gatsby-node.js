959773bbb1a147e061e74eb9383b9f3f3e64549d
Chris Santamaria <chris@santamaria.me>
2021-04-19 03:12:08 -0400

    Convert gatsby-node to ESM

--- src/gatsby-node.js
+++ src/gatsby-node.js
@@ -1,13 +1,13 @@
-const VirtualModulesPlugin = require('webpack-virtual-modules');
+import VirtualModulesPlugin from 'webpack-virtual-modules';
 
 // Based on CreateMediaConfig in https://github.com/artsy/fresnel/blob/master/src/Media.tsx
-exports.pluginOptionsSchema = ({ Joi }) =>
+export const pluginOptionsSchema = ({ Joi }) =>
   Joi.object({
     breakpoints: Joi.object().required().pattern(Joi.string(), Joi.number()),
     interactions: Joi.object().pattern(Joi.string(), Joi.string()),
   });
 
-exports.onCreateWebpackConfig = ({ actions }, pluginOptions) => {
+export const onCreateWebpackConfig = ({ actions }, pluginOptions) => {
   const moduleContents = JSON.stringify(pluginOptions);
 
   actions.setWebpackConfig({
